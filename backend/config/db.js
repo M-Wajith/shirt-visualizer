ddbdae1948be2ad5a07fb85173d71ced9eb3ec1f
M-Wajith <wajith.im@gmail.com>
2025-05-22 15:46:29 +0530

    db config updated for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
-const connectDB = async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGODB_URI,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        });
-        console.log('MongoDB Connected..');
-    }
-    catch(err){
-        console.error('MongoDB connection error: ',err);
-        process.exit(1)
-    }
+const connectDB = async () => {
+  try {
+    // Use production URI if it exists, otherwise fallback to local URI
+    const mongoURI = process.env.MONGODB_URI_PROD || process.env.MONGODB_URI;
+
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected..');
+  } catch (err) {
+    console.error('MongoDB connection error: ', err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
